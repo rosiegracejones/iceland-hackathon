ac3772270bd870beb620ede687ac6f5a4d0c927d
Rosie Jones <rosiegracejones@gmail.com>
2023-08-09 12:08:20 +0100

    page now displays selected prize and prize state added

--- iceland-spin/src/pages/PrizePage.jsx
+++ iceland-spin/src/pages/PrizePage.jsx
@@ -8,25 +8,27 @@
 
 import "./PrizePage.css";
 
-export default function PrizePage() {
+export default function PrizePage(prize, setPrize) {
 	const data = [
-		{ name: "Vegan Ice Cream", key: 1 },
-		{ name: "£100", key: 2 },
-		{ name: "Plant 20 trees", key: 3 },
-		{ name: "free delivery", key: 4 },
-		{ name: "10% off", key: 5 },
+		{ name: "Vegan Ice Cream", type: "item", discount: 0, key: 1 },
+		{ name: "£100", type: "cash", discount: 0, key: 2 },
+		{ name: "Plant 20 trees", type: "item", discount: 0, key: 3 },
+		{ name: "free delivery", type: "delivery", discount: 0, key: 4 },
+		{ name: "10% off", type: "discount", discount: 0.1, key: 5 },
 	];
 
 	function randomItem(data) {
 		let random = Math.floor(Math.random() * data.length);
 		let name = data[random].name;
 		console.log(name);
-		return name;
+    setPrize(name)
+		return `${name}`;
 	}
 
 	return (
 		<>
 			<button onClick={() => randomItem(data)}>Choose Random Prize</button>
+      <h1>{prize}</h1>
 		</>
 	);
 }
