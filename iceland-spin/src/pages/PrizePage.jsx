13467d1874ba6b37a0a8705cc9a9f4e812130297
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-08-09 12:53:15 +0100

    got a spin wheel

--- iceland-spin/src/pages/PrizePage.jsx
+++ iceland-spin/src/pages/PrizePage.jsx
@@ -2,51 +2,31 @@ import { useState } from 'react';
 import './PrizePage.css';
 
 const PrizePage = () => {
-  const [spinning, setSpinning] = useState(false);
-  const [result, setResult] = useState(null);
+  const [rotation, setRotation] = useState(0);
 
-  const prizes = [
-    "Free Item",
-    "50% Discount",
-    "10% Discount",
-    "Try Again",
-    "25% Discount",
-    "20% Discount",
-    "15% Discount",
-    "5% Discount",
-  ];
-
-  const spinWheel = () => {
-    if (!spinning) {
-      const randomIndex = Math.floor(Math.random() * prizes.length);
-      const spinDuration = 3000; // 3 seconds
-      
-      setSpinning(true);
-      setResult(null);
-      
-      setTimeout(() => {
-        setResult(prizes[randomIndex]);
-        setSpinning(false);
-      }, spinDuration);
-    }
+  const handleSpin = () => {
+    const randomNumber = Math.ceil(Math.random() * 10000);
+    setRotation(rotation + randomNumber);
   };
 
   return (
-    <div className="prize-page">
-      <div className={`wheel ${spinning ? 'spinning' : ''}`} onClick={spinWheel}>
-        <div className="wheel-inner">
-          <div className="wheel-section">
-            {prizes.map((prize, index) => (
-              <div className={`section-${index}`} key={index}>
-                {prize}
-              </div>
-            ))}
-          </div>
+    <div>
+      <title>Lucky Spin</title>
+      <body>
+        <div className="container"  onClick={handleSpin} style={{ transform: `rotate(${rotation}deg)` }}>
+          <div className="one">Like</div>
+          <div className="two">Comment</div>
+          <div className="three">Share</div>
+          <div className="four">Subscribe</div>
+          <div className="five">Support</div>
+          <div className="six">Visit</div>
         </div>
-      </div>
-      <div className="result">
-        {spinning ? 'Spinning...' : result}
-      </div>
+        <span className="mid"></span>
+        <button id="spin" onClick={handleSpin}>
+          Spin
+        </button>
+        <div className="stoper"></div>
+      </body>
     </div>
   );
 };
