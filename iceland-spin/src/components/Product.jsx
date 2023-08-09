592d3b9948f222149500488610a4b35b9d28a2a1
Grego12098 <jgregory12098@gmail.com>
2023-08-09 11:37:28 +0100

    add prepopulated basket

--- iceland-spin/src/components/Product.jsx
+++ iceland-spin/src/components/Product.jsx
@@ -1,11 +1,18 @@
 import products from "../data/products.json";
 
 export default function Product() {
+
+    const numbers = new Set();
+    for(let i = 0; i < products.length; i++) {
+        const random = Math.floor(Math.random() * products.length);
+        numbers.add(random);
+    }
+    const basket = [numbers];
+    console.log(basket);
     return (
         <>
-            <h1>Product Page</h1>
             <div className="product-container">
-                {products.map((product, index) => {
+                {basket.map((product, index) => {
                     return (
                         <div className="product-card" key={index}>
                             <img src={product.image} alt={product.title} />
--- iceland-spin/src/pages/CheckoutPage.jsx
+++ iceland-spin/src/pages/CheckoutPage.jsx
@@ -3,20 +3,11 @@ import Product from "../components/Product";
 export default function CheckoutPage() {
     return (
         <>
-            <h1>Checkout Page</h1>
+            <h1>Your Basket</h1>
             <Product />
         </>
     )
 }
 
 
-//     {"title": "Bio-D Laundry Liquid (Fragrence Free)", "price": , "image": },
-//     {"title": "Cheeky Panda Bamboo Kitcken Towel (pack of 2)", "price": , "image": },
-//     {"title": "Ecoffee cup - bonfrer (340mls)", "price": , "image": },
-//     {"title": "Ecoforce Recyclable Multi-purpose Clothes (twin pack)", "price": , "image": },
-//     {"title": "Loofco Washing Up Pads", "price": , "image": },
-//     {"title": "Organicup Menstrual Cup", "price": , "image": },
-//     {"title": "Reusable Sandwich Wrap", "price": , "image": },
-//     {"title": "The Naked Shave Kit", "price": , "image": },
-//     {"title": "Vegan Leather Lunch Bag", "price": , "image": }
 
