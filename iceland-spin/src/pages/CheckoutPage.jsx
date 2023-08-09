5c7c278b71e1734a30612ba6686759dfca8ba089
Grego12098 <jgregory12098@gmail.com>
2023-08-09 13:38:58 +0100

    commneted out strech goal code

--- iceland-spin/src/pages/CheckoutPage.jsx
+++ iceland-spin/src/pages/CheckoutPage.jsx
@@ -4,20 +4,14 @@ export default function CheckoutPage() {
     return (
         <div className="basket">
              <h1>Your Basket</h1>
-
-        <div className="item">
-            <div className="buttons">
-                <button className="delete-button" type="button">Delete</button>
-            </div>
-        </div>    
        
             <Product/>
 
-         <div className="quantity">
+         {/* <div className="quantity">
             <button className="plus-button" type="button" name="button">-</button>
             <input type="text" name="name" value="1"/>
             <button className="minus-button" type="button" name="button">+</button>
-        </div>
+        </div> */}
 
         </div>
     )
