d24f5e096aef0f19aeb3d69b10a64879ef966baa
Evangeline Amoura <evangelineamoura@yahoo.com>
2023-08-09 12:22:45 +0100

    elements for checkout page added

--- iceland-spin/src/pages/CheckoutPage.jsx
+++ iceland-spin/src/pages/CheckoutPage.jsx
@@ -2,13 +2,30 @@ import Product from "../components/Product";
 
 export default function CheckoutPage() {
     return (
-        <>
+        <div className="basket">
             <h1>Checkout Page</h1>
-            <Product />
-        </>
+
+        <div className="item">
+            <div className="buttons">
+                <button className="delete-button" type="button">Delete</button>
+            </div>
+        </div>    
+
+        <div className="product">
+            <Product/>
+         </div>
+
+         <div className="quantity">
+            <button className="plus-button" type="button" name="button">-</button>
+            <input type="text" name="name" value="1"/>
+            <button className="minus-button" type="button" name="button">+</button>
+        </div>
+
+        </div>
+        
     )
 }
-
+      
 
 //     {"title": "Bio-D Laundry Liquid (Fragrence Free)", "price": , "image": },
 //     {"title": "Cheeky Panda Bamboo Kitcken Towel (pack of 2)", "price": , "image": },
